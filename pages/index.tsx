0052124b2f98dbacf2fd49e8ad501d9d907957cd
Edward Lanto <elanto@hotmail.com>
2020-11-13 19:37:04 -0800

    finished api call to yelp, passed through getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 const axios = require('axios');
-import type { AppProps /*, AppContext */ } from 'next/app';
 
 export default function Home({ data }) {
+  const category = data.data.businesses.filter((x) => x.rating >= 4.5);
   return (
     <div className="md:flex bg-white rounded-lg p-24 justify-center">
       <div className="text-center md:text-left">Home</div>
@@ -11,9 +11,9 @@ export default function Home({ data }) {
 
 // This gets called on every request
 export async function getStaticProps(context) {
-  try {
-    const endpoint = 'https://api.yelp.com/v3/businesses/search';
-    const data = await axios.get('https://api.yelp.com/v3/businesses/search', {
+  const { data } = await axios.get(
+    'https://api.yelp.com/v3/businesses/search',
+    {
       params: {
         term: 'restaurants',
         latitude: 49.218739,
@@ -24,20 +24,11 @@ export async function getStaticProps(context) {
       headers: {
         Authorization: `Bearer WeqfeX1FQNyYkNH7ggVueKDZEvSDn3zT2tnr4gYMC6siINdeFsCKY31LPUmQN-ka48xhCFYSIgN3wC9AB1671WxnNrzjAkLgMM6iQau-CDI-2V-iZTE06sPT86MxX3Yx`,
       },
-    });
-    let categories;
-    const businesses = data.data.businesses
-      .filter((x) => x.rating >= 4.5)
-
-      // Create array of categories
-      .map((x) => x.categories);
-
-    console.log('data', data.data);
-    console.log('businsses', businesses);
-
-    // Pass data to the page via props
-    return { props: { test: 'test' } };
-  } catch (err) {
-    return err;
-  }
+    }
+  );
+  return {
+    props: {
+      data: data,
+    },
+  };
 }
