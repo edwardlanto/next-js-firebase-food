b24e54935efa36079b177a8b5a16cd38fb926a73
Edward Lanto <elanto@hotmail.com>
2020-11-16 00:24:55 -0800

    axios call working

--- helpers/categories.tsx
+++ helpers/categories.tsx
@@ -1,54 +1,48 @@
-const axios = require('axios');
-const Category = require('../classes/category');
-
+const axios = require('axios')
+const Category = require('../classes/category')
+import Unsplash, { toJson } from 'unsplash-js'
+const unsplash = new Unsplash({ accessKey: process.env.UNSPLASH_API_KEY })
 export async function categories() {
-  const { data } = await axios.get(
-    'https://api.yelp.com/v3/businesses/search',
-    {
-      params: {
-        term: 'restaurants',
-        latitude: 49.218739,
-        longitude: -123.082031,
-        limit: 1,
-        radius: 5000,
-      },
-      headers: {
-        Authorization: `Bearer ${process.env.YELP_API_KEY}`,
-      },
+    const { data } = await axios.get(
+        'https://api.yelp.com/v3/businesses/search',
+        {
+            params: {
+                term: 'restaurants',
+                latitude: 49.218739,
+                longitude: -123.082031,
+                limit: 40,
+                radius: 5000,
+            },
+            headers: {
+                Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+            },
+        }
+    )
+
+    let businesses = data.businesses.filter((x) => x.rating >= 4.5)
+
+    let categories = businesses.map((x) => x.categories[0])
+
+    async function loopImages() {
+        let arr = []
+        for (let i = 0; i < businesses.length; i++) {
+            for (var y = 0; y < businesses.length; y++) {
+                const url = `https://api.unsplash.com/search/photos?query=coffee&per_page=2&client_id=${process.env.UNSPLASH_API_KEY}`
+                await unsplash.search
+                    .photos('dogs', 1, 10, {
+                        orientation: 'portrait',
+                        color: 'green',
+                    })
+                    .then(toJson)
+                    .then((res) => {
+                        businesses[i].image = res.results[0].urls.small
+                    })
+            }
+        }
     }
-  );
 
-  let categories = data.businesses
-    .filter((x) => x.rating >= 4.5)
-    .map((x) => x.categories);
-  let arr = [];
-
-  categories.map(async (x) =>
-    x.map((data, i) => {
-      let obj = {
-        image: null,
-        title: data.title,
-        alias: data.alias,
-        count: 0,
-      };
-
-      const url = `https://api.unsplash.com/search/photos?query=coffee&per_page=2&client_id=${process.env.UNSPLASH_API_KEY}`;
-      fetch(url)
-        .then((response) => {
-          return response.json();
-        })
-        .then((res) => {
-          obj.image = res.results[0].urls.small;
-          console.log('title', obj);
-        });
-
-      if (arr.findIndex((item) => item.title === data.title) === -1) {
-        arr.push(obj);
-      } else {
-        console.log('found');
-      }
-    })
-  );
-
-  return arr;
+    return {
+        businesses,
+        categories,
+    }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
-const axios = require('axios');
-import { categories } from '../helpers/categories';
+const axios = require('axios')
+import { categories } from '../helpers/categories'
 
 export default function Home({ data }) {
-  return (
-    <div className="md:flex bg-white rounded-lg p-24 justify-center">
-      <div className="text-center md:text-left">Home</div>
-    </div>
-  );
+    return (
+        <div className="md:flex bg-white rounded-lg p-24 justify-center">
+            <div className="text-center md:text-left">Home</div>
+        </div>
+    )
 }
 
 // This gets called on every request
 export async function getStaticProps(context) {
-  const data = await categories();
-  console.log('data', data);
-  return {
-    props: {
-      data: 'test',
-    },
-  };
+    const data = await categories()
+    console.log('data', data)
+    return {
+        props: {
+            data: data,
+        },
+    }
 }
