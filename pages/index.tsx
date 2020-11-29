1980a2d531e5a75390ddfaa0de1f949fe6b3ab3c
Edward Lanto <edwardlanto.developer@gmail.com>
2020-11-28 21:54:14 -0800

    fixed error comments next js home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
 const axios = require('axios')
 import { categories } from '../helpers/categories'
-
+import Layout from '../components/layout'
 export default function Home({ data }) {
-    return (
-        <div className="md:flex bg-white rounded-lg p-24 justify-center">
-            <div className="text-center md:text-left">Home</div>
-        </div>
-    )
+    return <div>Home</div>
 }
 
 // This gets called on every request
